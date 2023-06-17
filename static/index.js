06562459285175f30fa20986aa0f0708ff27a58a
Alby1 <alby.macchi2004@gmail.com>
2023-06-17 20:03:23 +0200

    effetto figo verticale

--- static/index.js
+++ static/index.js
@@ -1,9 +1,16 @@
 function onLoad() {
     document.addEventListener("scroll", pageOnScroll)
     pageOnScroll()
+    /* document.addEventListener("mousemove", mouseOnMove, {passive: true})
+    SWidth = 100
+    SHeight = 100
+    shadow = document.getElementById("mouse_shadow")
+    shadow.style.width = SWidth + "px"
+    shadow.style.height = SHeight + "px" */
 }
 
 function pageOnScroll(event) {
+    // return // boh forse ci torno
     if (window.innerWidth > 730) return
     let elements = document.getElementsByClassName("flex-item")
     for (el of elements) {
@@ -11,11 +18,21 @@ function pageOnScroll(event) {
         const wHeight = window.innerHeight
         const wHeight3 = wHeight / 3
         const wHeight32 = wHeight3 * 2
-        /* if(elY < wHeight3 || elY > wHeight32) continue */
+        // if(elY < wHeight3 || elY > wHeight32) {
+        //     el.style.width = "85%"
+        //     continue
+        // }
         const clampNumber = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
-        el.style.width = `${85 + clampNumber(wHeight32 / elY * 20, 0, 20)}%`
+        let factor = (elY < wHeight3 ? (elY / wHeight3) : (wHeight32 / elY))
+        el.style.width = `${70 + clampNumber(factor * 20, 0, 20)}%`
+        /* TODO: fare ↑ ma col padding */
         
+        // el.style.padding = ``
     }
-    console.log("--------------")
-    
+}
+
+function mouseOnMove(event) {
+    shadow.style.left = `${event.pageX-(SWidth/2)}px`
+    shadow.style.top = `${event.pageY-(SHeight/2)}px`
+    let eFP = document.elementFromPoint(event.clientX, event.clientY)
 }
