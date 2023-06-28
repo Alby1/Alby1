bcd74f21719092ff88616e7b3bcc7a7d67f61baf
Alby1 <alby.macchi2004@gmail.com>
2023-06-29 00:02:06 +0200

    add: last.fm statistics

--- server.py
+++ server.py
@@ -4,12 +4,18 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
 import json
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 async def root():
@@ -18,6 +24,27 @@ async def root():
 def get_file(lan: str) -> dict:
     with open(f"{lan.lower()}.json", "r", encoding="utf8") as file:
         return json.load(file)
+    
+
+@app.get("/lastfm/user")
+async def lastfm_user():
+    return requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=malbyx&api_key={os.getenv('LAST_FM_KEY')}&format=json").json()
+
+@app.get("/lastfm/recent_tracks")
+async def lastfm_recent_tracks():
+    return requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=malbyx&api_key={os.getenv('LAST_FM_KEY')}&format=json&limit=5&extended=1").json()
+
+@app.get("/lastfm/top_artists")
+async def lastfm_top_artists():
+    return requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=malbyx&api_key={os.getenv('LAST_FM_KEY')}&format=json&limit=5").json()
+
+@app.get("/lastfm/top_tracks")
+async def lastfm_top_tracks():
+    return requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=rj&api_key={os.getenv('LAST_FM_KEY')}&format=json&limit=5&period=overall").json()
+
+@app.get("/lastfm/weekly_top_tracks")
+async def lastfm_weekly_top_tracks():
+    return requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart&user=malbyx&api_key={os.getenv('LAST_FM_KEY')}&format=json&limit=5").json()
 
 
 @app.get("/{lan}", response_class=HTMLResponse)
@@ -32,6 +59,7 @@ async def read_item(request: Request, lan: str):
     obj["request"] = request
     obj["lan"] = lan
     return templates.TemplateResponse("index.html", obj)
+
 
 if __name__ == "__main__":
     uvicorn.run("server:app", reload=True, port=5002, host="0.0.0.0")
--- static/index.js
+++ static/index.js
@@ -7,9 +7,111 @@ function onLoad() {
     shadow = document.getElementById("mouse_shadow")
     shadow.style.width = SWidth + "px"
     shadow.style.height = SHeight + "px" */
-    let img = document.getElementById("img")
-    console.log(img.complete, img.naturalHeight)
+    /* let img = document.getElementById("img")
+    console.log(img.complete, img.naturalHeight) */
+
+
+
+    getData(`${location.origin}/lastfm/top_artists`, (data) => {
+        let top_artists = document.getElementById("top_artists")
+        top_artists.innerHTML = top_artists.innerHTML.replace("{count}", data["topartists"]["@attr"]["perPage"])
+        let top_artists_ = top_artists.innerHTML
+        for (let artist of data["topartists"]["artist"]) {
+            let a = document.createElement("a")
+            let li = document.createElement("li")
+            
+            li.innerHTML = `${artist["@attr"]["rank"]}: `
+            a.innerHTML = `${artist["name"]}`
+            a.href = artist["url"]
+            li.appendChild(a)
+            li.innerHTML += ` (${artist["playcount"]})`
+            top_artists.appendChild(li)
+        }
+
+        if (top_artists.innerHTML != top_artists_) top_artists.hidden = false
+        
+    })
+
+    getData(`${location.origin}/lastfm/top_tracks`, (data) => {
+        let top_tracks = document.getElementById("top_tracks")
+        top_tracks.innerHTML = top_tracks.innerHTML.replace("{count}", data["toptracks"]["@attr"]["perPage"])
+        let top_tracks_ = top_tracks.innerHTML
+        for (let track of data["toptracks"]["track"]) {
+            let a = document.createElement("a")
+            let li = document.createElement("li")
+            li.innerHTML = `${track["@attr"]["rank"]}: `
+            a.href = track["url"]
+            a.innerHTML = `${track["name"]} - ${track["artist"]["name"]}`
+            li.appendChild(a)
+            li.innerHTML += ` (${track["playcount"]})`
+            top_tracks.appendChild(li)
+        }
+
+        if (top_tracks.innerHTML != top_tracks_) top_tracks.hidden = false
+    })
+
+    getData(`${location.origin}/lastfm/weekly_top_tracks`, (data) => {
+        let weekly_top_tracks = document.getElementById("weekly_top_tracks")
+        let weekly_top_tracks_ = weekly_top_tracks.innerHTML
+        for (let track of data["weeklytrackchart"]["track"]) {
+            let a = document.createElement("a")
+            let li = document.createElement("li")
+            li.innerHTML = `${track["@attr"]["rank"]}: `
+            a.href = track["url"]
+            a.innerHTML = `${track["name"]} - ${track["artist"]["#text"]}`
+            li.appendChild(a)
+            li.innerHTML += ` (${track["playcount"]})`
+            weekly_top_tracks.appendChild(li)
+        }
+
+        if (weekly_top_tracks.innerHTML != weekly_top_tracks_) weekly_top_tracks.hidden = false
+    })
     
+    getData(`${location.origin}/lastfm/user`, (data) => {
+        let music_info_desc = document.getElementById("music_info_desc")
+        for (let info of Object.keys(data["user"])) {
+            if (info == "registered") data["user"][info] = (new Date(Date(data["user"][info]["#text"]))).toUTCString()
+            music_info_desc.innerHTML = music_info_desc.innerHTML.replace(`{${info}}`, data["user"][info])
+        }
+        let music_info_link = document.getElementById("music_info_link")
+        music_info_link.href = data["user"]["url"]
+    })
+
+    loadRecentTracks()
+}
+
+async function loadRecentTracks() {
+    while(true) {
+        getData(`${location.origin}/lastfm/recent_tracks`, (data) => {
+            let listening_to = document.getElementById("listening_to")
+            let last_listened_to = document.getElementById("last_listened_to")
+            let song = null
+            for (let i in data["recenttracks"]["track"]) {
+                let song_ = data["recenttracks"]["track"][i]
+                if(song_["@attr"] && song_["@attr"]["nowplaying"] == "true") {
+                    listening_to.innerHTML = `${song_["name"]} - ${song_["artist"]["name"]}`
+                    listening_to.href = song_["url"]
+                    listening_to.parentNode.hidden = false
+    
+                    let j = Number(i)+1
+                    song = data["recenttracks"]["track"][j]
+                    break
+                }
+            }
+
+            if(last_listened_to.hidden) {
+                song_ = data["recenttracks"]["track"][0]
+            }
+            
+            if(song != null) {
+                last_listened_to.innerHTML = `${song["name"]} - ${song["artist"]["name"]}`
+                last_listened_to.href = song["url"]
+                last_listened_to.parentNode.hidden = false
+            }
+            
+        })
+        await new Promise(r => setTimeout(r, 120000));
+    }
 }
 
 function pageOnScroll(event) {
@@ -22,7 +124,9 @@ function pageOnScroll(event) {
         const wHeight3 = wHeight / 5
         const wHeight32 = wHeight / 10 * 6
         const clampNumber = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
-        let factor = (elY < wHeight3 ? (elY / wHeight3) : (wHeight32 / elY))
+        // devo mettere  + el.offsetHeight da qualche parte
+        //                              chiusura        : apertura
+        let factor = (elY < wHeight3 ? (elY / wHeight3 + el.offsetHeight) : (wHeight32 / elY))
         el.style.width = `${70 + clampNumber(factor * 20, 0, 20)}%`
     }
 }
@@ -32,3 +136,9 @@ function mouseOnMove(event) {
     shadow.style.top = `${event.pageY-(SHeight/2)}px`
     let eFP = document.elementFromPoint(event.clientX, event.clientY)
 }
+
+async function getData(url, func) {
+    let data = await fetch(url)
+    if(data.status == 200) func(await data.json())
+}
+
